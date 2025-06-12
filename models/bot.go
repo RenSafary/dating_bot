f1eca1dba6fd505d27cc36cd4882f3fc1bef5771
RenSafary <rensafary@gmail.com>
2025-06-12 15:17:05 +0500

    watched profile

--- models/bot.go
+++ models/bot.go
@@ -8,18 +8,20 @@ import (
 )
 
 type BotHandlers struct {
-	Bot      *telebot.Bot
-	Db       *sql.DB
-	DataUser map[string]string
-	States   map[int64]string
-	Mu       sync.Mutex
+	Bot              *telebot.Bot
+	Db               *sql.DB
+	DataUser         map[string]string
+	States           map[int64]string
+	Mu               sync.Mutex
+	CurrentProfileID map[int64]int64
 }
 
 func New(bot *telebot.Bot, db *sql.DB) *BotHandlers {
 	return &BotHandlers{
-		Bot:      bot,
-		Db:       db,
-		DataUser: make(map[string]string),
-		States:   make(map[int64]string),
+		Bot:              bot,
+		Db:               db,
+		DataUser:         make(map[string]string),
+		States:           make(map[int64]string),
+		CurrentProfileID: make(map[int64]int64),
 	}
 }
